876c515de18a9aa8c5f3e41a00d5ff659a825b7f
Wanying Song <songwanying2015@gmail.com>
2024-04-18 20:20:58 -0400

    More on Cart and HomePage

---
+++ client/src/components/CartItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useCart } from './CartContext';
+
+const CartItem = ({ item }) => {
+    const { dispatch } = useCart();
+
+    const handleRemoveItem = () => {
+    dispatch({ type: 'REMOVE_ITEM', payload: { id: item.id } });
+    };
+
+    const handleQuantityChange = (e) => {
+        const quantity = parseInt(e.target.value);
+        dispatch({ type: 'UPDATE_QUANTITY', payload: { id: item.id, quantity } });
+    };
+
+    return (
+    <div>
+        <img src={item.imageSrc} alt={item.name} />
+        <p>{item.name} - ${item.price}</p>
+        <input type="number" value={item.quantity} onChange={handleQuantityChange} />
+        <button onClick={handleRemoveItem}>Remove</button>
+    </div>
+    );
+    };
+
+export default CartItem;
